b607a73dc2492f91548c5e04e922d5e8c8c28812
mgoldchild <mgoldchild@gmail.com>
2019-03-31 18:06:30 +0900

    Modify train.sh

--- struct2depth/train.sh
+++ struct2depth/train.sh
@@ -2,13 +2,13 @@
 
 ckpt_dir="/home/mike/apps/vadl-training/struct2depth/checkpoints"
 data_dir="/home/mike/apps/vadl-training/struct2depth/processed_ss2_data" # Set for Cityscapes
-imagenet_ckpt="/home/mike/apps/vadl-training/struct2depth/struct2depth_model_kitti/model-199160"
+# imagenet_ckpt="/home/mike/apps/vadl-training/struct2depth/struct2depth_model_kitti/model-199160"
 
 python train.py \
   --logtostderr \
   --checkpoint_dir $ckpt_dir \
-  --data_dir $data_dir \
+  --data_dir $data_dir # \
   # --architecture resnet \
-  --imagenet_ckpt $imagenet_ckpt #\
+  # --imagenet_ckpt $imagenet_ckpt #\
   # --imagenet_norm true \
   # --joint_encoder false
